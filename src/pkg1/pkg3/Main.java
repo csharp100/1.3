45d1fbbb6209804666d451046cf0ceb12ef9b884
csharp100 <clint63012@hotmail.com>
2014-03-16 09:51:02 -0500

    Exercise 1.3 Liang Java 8E Comprehensive Version Initial Commit

---
+++ src/pkg1/pkg3/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Exercise 1.3 from the Liang Java Textbook 8E
+ * Comprehensive Version
+ */
+
+package pkg1.pkg3;
+
+/**
+ *
+ * @author Clint Sharp
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("    J     A    V     V    A");
+        System.out.println("    J    A A    V   V    A A");
+        System.out.println("J   J   AAAAA    V V    AAAAA");
+        System.out.println(" J J   A     A    V    A     A");
+    }
+    
+}
